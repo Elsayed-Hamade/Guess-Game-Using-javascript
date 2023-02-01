6e607e575d5fa77626223a8106653fa5aab834df
Elsayed Hamada <elsayed.7work@gmail.com>
2023-02-01 22:38:00 +0200

    Done with styling

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// Import Classes
+/* Light & Dark Mode */
+const light = document.querySelector(".light");
+const dark = document.querySelector(".dark");
+const container = document.querySelector(".container");
+const logoImg = document.querySelector(".logo-img");
+
+
+light.addEventListener("click", ()=>{
+    document.body.classList.add("active");
+    logoImg.src = "Assets/logoLight.png"
+    
+});
+
+dark.addEventListener("click", ()=>{
+    logoImg.src = "Assets/logoDark.png"
+    document.body.classList.remove("active");
+});
+
+
+// Game Functions
